4b99206b26756df06439f40827cf36886577b118
ashvin <123237841+ashvxn@users.noreply.github.com>
2025-02-09 21:21:07 +0530

    api token salt added

--- config/admin.js
+++ config/admin.js
@@ -3,13 +3,15 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('cdJrmvJQeqM+3FccjcDa3w=='),
   },
+  
   transfer: {
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
+  
   flags: {
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
